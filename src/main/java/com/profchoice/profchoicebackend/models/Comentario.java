be4911c9b3cdf72a4765fc31c55ec9a7b5cd8d8d
FelipeVitta <feliperiku@gmail.com>
2021-11-11 17:24:59 -0300

    thread-java e comentario-java

--- src/main/java/com/profchoice/profchoicebackend/models/Comentario.java
+++ src/main/java/com/profchoice/profchoicebackend/models/Comentario.java
@@ -2,16 +2,53 @@ package com.profchoice.profchoicebackend.models;
 
 public class Comentario {
 
+  private int id_thread;
+  private int id_comentario;
+  private String autorComentario;
+  private String comentario;
 
- private int id_thread;
- private int id_comentario;
- private String autorComentario;
- private String comentario;
+  public Comentario() {
 
-  public Comentario(){
-      
   }
 
+  public Comentario(int id_thread, int id_comentario, String autorComentario, String comentario) {
+    this.id_comentario = id_comentario;
+    this.id_comentario = id_comentario;
+    this.autorComentario = autorComentario;
+    this.comentario = comentario;
 
+  }
+
+  public int getId_thread() {
+    return id_thread;
+  }
+
+  public void setId_thread(int id_thread) {
+    this.id_thread = id_thread;
+  }
+
+  public int getId_comentario() {
+    return id_comentario;
+  }
+
+  public void setId_comentario(int id_comentario) {
+    this.id_comentario = id_comentario;
+  }
+
+  public String getAutorComentario() {
+    return autorComentario;
+  }
+
+  public void setAutorComentario(String autorComentario) {
+    this.autorComentario = autorComentario;
+  }
+
+  public String getComentario() {
+    return comentario;
+  }
+
+  public void setComentario(String comentario) {
+    this.comentario = comentario;
+  }
 
 }
--- src/main/java/com/profchoice/profchoicebackend/models/Thread.java
+++ src/main/java/com/profchoice/profchoicebackend/models/Thread.java
@@ -1,16 +1,45 @@
 package com.profchoice.profchoicebackend.models;
 
 public class Thread {
-    
+
   private int id_thread;
   private String conteudo;
   private String authorThread;
 
-  public Thread(){
-
-
+  public Thread() {
 
   }
 
+  public Thread(int id_thread, String conteudo, String authorThread) {
+
+    this.id_thread = id_thread;
+    this.conteudo = conteudo;
+    this.authorThread = authorThread;
+
+  }
+
+  public int getId_thread() {
+    return id_thread;
+  }
+
+  public void setId_thread(int id_thread) {
+    this.id_thread = id_thread;
+  }
+
+  public String getConteudo() {
+    return conteudo;
+  }
+
+  public void setConteudo(String conteudo) {
+    this.conteudo = conteudo;
+  }
+
+  public String getAuthorThread() {
+    return authorThread;
+  }
+
+  public void setAuthorThread(String authorThread) {
+    this.authorThread = authorThread;
+  }
 
 }
